eb6bc88a797b4de0d978596e05f247c8ef21e607
kellyjiang18 <99273147+kellyjiang18@users.noreply.github.com>
2023-01-18 13:52:37 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ const courses = [
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
 });
+
 //request courses by id
 app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find(c=> c.id === parseInt(req.params.id));
@@ -30,14 +32,25 @@ app.listen(3000,()=>{
 
 //http POST requests
 app.post('/api/courses',(req,res)=>{
-    if(req.body.name.length<3){
-        res.status(404).send("Name is required to be a minimum for 3 characters");  
+    if(req.body.name.length>=3)
+    {
+        const course ={
+            id: courses.length+1,
+            name:req.body.name
+        }
+        courses.push(course);
+        res.send(course);
+    }
+    res.status(404).send("The courses name needs to be 3 or more characters long");
+});
+
+//http PUT requests
+app.put('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send("The courses with the given ID was not found");
         return
     }
-    const course = {
-        id: courses.length+1,
-        name:req.body.name
-    }
-    courses+=course;
-    res.send(course);
+    courses[courses.findIndex(course)].name=req.body.name;
+    res.send(courses[courses.findIndex(course)]);
 });
